a6bee64c96245ab4c2ea67349df919ef4b6e1fd3
muhammadfiaz12263 <89456388+muhammadfiaz12263@users.noreply.github.com>
2023-10-15 04:12:50 +0000

    datatypes part1 done

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,14 @@
+"use strict" //treat all JS code as new version
+
+// number => 123
+// bigint 
+// string => "hi"
+// null => standalone value
+// boolean => true/false
+// undefined => 
+// symbol => unique
+
+console.log(typeof "Hi"); //string
+console.log(typeof 123) //number
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
