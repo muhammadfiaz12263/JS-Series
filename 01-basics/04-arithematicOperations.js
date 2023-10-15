95a25114e9148809be91fae3d58f2277426cc775
muhammadfiaz12263 <89456388+muhammadfiaz12263@users.noreply.github.com>
2023-10-15 16:18:05 +0000

    Arithematic Operations Done

---
+++ 01-basics/04-arithematicOperations.js
@@ -0,0 +1,33 @@
+// let str1 = "hiiiii"
+// let str2 = "faizy"
+// let str3 = str1 + " " + str2;
+// console.log(str3);
+
+// let val = 3;
+// let negVal = -val
+// console.log(negVal); //-3
+
+// console.log(2+2);//4   
+// console.log(2-2);//0
+// console.log(-2);//-2
+// console.log(2*2);//4
+// console.log(2/2);//1
+// console.log(2%3);
+// console.log(3%2);
+
+// console.log(+true);//1
+// console.log(true+);//1
+// console.log(-true);//-1
+// console.log(+false);//0
+// console.log(-false);//-0
+// console.log(+"");//0
+// console.log(+"hiii");//NaN
+
+// console.log("1" +  2);//12
+// console.log(1 +  "2");//12
+// console.log("1" +  2 + 3);//123
+// console.log(1 +  2 + "3");//33
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gamneCounter);//101
