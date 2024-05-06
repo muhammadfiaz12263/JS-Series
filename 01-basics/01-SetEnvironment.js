2d8e21939e3ceddd0f0e92cfff08a4f68a015192
muhammadfiaz12263 <89456388+muhammadfiaz12263@users.noreply.github.com>
2024-05-06 17:31:05 +0000

    Set Environment Of JS

--- 01-basics/01-SetEnvironment.js
+++ 01-basics/01-SetEnvironment.js
@@ -1,1 +1,5 @@
-console.log("Helloooo World");
+/*
+1. CLick on 3 LINES and Go to FILE then View and then Go to COMMAND PALETTE or press CTRL+SHIFT+P
+2. Write CONTAINER in command palette and click on add DEV CONTAINER
+3. Write Node and click on show all definitions then click on Node & JS
+*/
