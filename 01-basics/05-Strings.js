7d60d0e992104a5997e0670d25432bb35dee7e17
muhammadfiaz12263 <89456388+muhammadfiaz12263@users.noreply.github.com>
2024-05-28 06:04:37 +0000

    Strings1-

--- 01-basics/05-Strings.js
+++ 01-basics/05-Strings.js
@@ -119,8 +119,26 @@ function checkPasswordStrength(password) {
     }
 }
 
-console.log(checkPasswordStrength("Passw0rd!")); // Outputs: Strong
-console.log(checkPasswordStrength("password"));  // Outputs: Weak
+// console.log(checkPasswordStrength("Passw0rd!")); // Outputs: Strong
+// console.log(checkPasswordStrength("password"));  // Outputs: Weak
+
+
+//---2. Text Encoding
+
+function stringToBase64(str) {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        let binary = str.charCodeAt(i).toString(2);
+        result += binary.padStart(8, '0');
+        console.log(binary.padStart(8, '0'));
+    }
+    //return btoa(result);
+    return (result);
+}
+
+console.log(stringToBase64("Hello")); // Outputs: "0100100001100101011011000110110001101111"
+
+console.log(btoa(stringToBase64("Hello")));
 
 
 
